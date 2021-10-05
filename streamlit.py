f22a97a3696d2897ab3cc6742a71e4b45a9bbe54
SundarAnand <43403815+SundarAnand@users.noreply.github.com>
2021-10-05 18:17:18 -0400

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.write(""" 
+# HELLO WORLD 
+***How*** are you? """)
+
+number = st.slider("Pick a number", 0, 100)
+
+date = st.date_input("Pick a date")
+
+st.write("""
+You have chosen """ + str(number) + """ number and """ + str(date) + """ date
+""")
