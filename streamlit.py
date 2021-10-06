0be9cc3ed80e808053834bce6809916ce5fbcf40
Sundar <43403815+SundarAnand@users.noreply.github.com>
2021-10-06 18:02:37 -0400

    Added some comments and viz

--- streamlit.py
+++ streamlit.py
@@ -1,40 +1,58 @@
+# Required libraries
 import streamlit as st
 import pandas as pd
-import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+
+# Introduction
 st.write(""" 
-# HELLO WORLD 
-How are you? """)
+# Interactive Data Science
+## By Annie Johnson and Sundar Anand
+## Interesting trends when pandemic happened""")
 
-number = st.slider("Pick a number", 0, 100)
+# Reading the file
+country_df = pd.read_csv("vaccination dataset/country.csv")
 
-date = st.date_input("Pick a date")
+# Getting only vaccination data
+vaccine_df = country_df[(country_df['people_vaccinated'].notna())]
+vaccine_df['date'] = pd.to_datetime(vaccine_df['date']).dt.date
+vaccine_df = vaccine_df.sort_values(by='date')
 
-st.write("""
-You have chosen """ + str(number) + """ number and """ + str(date) + """ date
-""")
+# Getting the country list
+country_list = sorted(vaccine_df['country'].unique())
 
-#country = pd.read_csv("vaccination dataset/country.csv")
+# Getting country and date as input
+option = st.selectbox('Pick a country', country_list)
+country_vaccine_df = vaccine_df[(vaccine_df['country'] == option)]
+date = st.select_slider("Pick a date", options = country_vaccine_df['date'].unique())
 
-data = pd.DataFrame({
-    'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
-    'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
-    'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
-})
+# Calculating the percentage of vaccination
 
-# Adding code so we can have map default to the center of the data
-midpoint = (np.average(data['lat']), np.average(data['lon']))
+vacc_info_df = country_vaccine_df[(country_vaccine_df['date'] == pd.to_datetime(str(date)))]
+vacc = vacc_info_df['people_vaccinated'].iloc[0]
+per = round(vacc*100/vacc_info_df['population'].iloc[0], 2)
 
-st.deck_gl_chart(
-            viewport={
-                'latitude': midpoint[0],
-                'longitude':  midpoint[1],
-                'zoom': 4
-            },
-            layers=[{
-                'type': 'ScatterplotLayer',
-                'data': data,
-                'radiusScale': 250,
-   'radiusMinPixels': 5,
-                'getFillColor': [248, 24, 148],
-            }]
-        )
+# Displaying the results
+st.write("The total vaccinated people are " + str(vacc) + " and the vaccination percentage as of " + str(date) + " is " + str(per) + "%")
+
+# Country wise comparison bar graphs
+vaccine_df['date'] = pd.to_datetime(vaccine_df['date']).dt.strftime('%Y-%m-%d')
+countries = st.multiselect("Which country do you like to see?", country_list, ['United States'])
+date = st.selectbox("Pick a date", options = ['autoplay'] + list(vaccine_df[vaccine_df['country'].isin(countries)]['date'].unique()))
+
+# For a selected date
+if date != 'autoplay':
+    conditioned_df = vaccine_df[(vaccine_df['country'].isin(countries)) & (vaccine_df['date'] == pd.to_datetime(date))]
+    fig = px.bar(conditioned_df, x='country', y='people_vaccinated', color='country')
+
+# For all dates
+else:
+    conditioned_df = vaccine_df[(vaccine_df['country'].isin(countries))]
+    #conditioned_df['date'] = pd.to_datetime(conditioned_df['date']).dt.strftime('%Y-%m-%d')
+    fig = px.bar(conditioned_df, x='country', y='people_vaccinated', range_y=[0, max(conditioned_df['people_vaccinated'])], color='country', animation_frame='date', animation_group='country')
+    fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 10
+    fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 4
+    fig.update_layout(width=1000)
+
+# Displaying the graph
+st.write(fig)
