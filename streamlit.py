495457ed7670b0a4e428b423b0aa4403cb79739b
Sundar <43403815+SundarAnand@users.noreply.github.com>
2021-10-07 02:10:19 -0400

    added more viz

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,6 @@
 # Required libraries
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
 
 # Introduction
@@ -15,11 +14,55 @@ country_df = pd.read_csv("vaccination dataset/country.csv")
 country_df['date'] = pd.to_datetime(country_df['date']).dt.date
 
 # Getting only vaccination data
-vaccine_df = country_df[(country_df['people_vaccinated'].notna())]
+vaccine_df = country_df[(country_df['percentage_vaccinated'].notna()) & (country_df['percentage_active'].notna())]
 vaccine_df = vaccine_df.sort_values(by='date')
 
 # Getting the country list
 country_list = sorted(vaccine_df['country'].unique())
+
+st.write("""
+#### Let's compare the vaccination timeseries data between countries at once...
+""")
+
+# Country wise comparison bar graphs
+countries = st.multiselect("Which countries do you like to see?", country_list, ['United States', 'India', 'Brazil'])
+date = st.selectbox("Pick a date", options = ['autoplay'] + list(vaccine_df[vaccine_df['country'].isin(countries)]['date'].unique()))
+
+# For a selected date
+if date != 'autoplay':
+    conditioned_df = vaccine_df[(vaccine_df['country'].isin(countries)) & (vaccine_df['date'] == pd.to_datetime(date))]
+    fig = px.bar(conditioned_df, x='country', y=['percentage_vaccinated', 'percentage_active'], barmode='group')
+    st.plotly_chart(fig, use_container_width=False)
+
+# For all dates
+else:
+    conditioned_df = vaccine_df[(vaccine_df['country'].isin(countries))]
+    conditioned_df['date'] = pd.to_datetime(conditioned_df['date']).dt.strftime('%Y-%m-%d')
+    
+    # # Vaccination plot
+    # fig1 = px.bar(conditioned_df, x='country', y='percentage_vaccinated', color='country', range_y=[0,100], animation_frame='date', animation_group='country')
+    # fig1.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 10
+    # fig1.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 4
+
+    # # Count plot
+    # fig2 = px.bar(conditioned_df, x='country', y='percentage_active', color='country', range_y=[0,10], animation_frame='date', animation_group='country')
+    # fig2.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 10
+    # fig2.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 4
+
+    # # Displaying the graph
+    # fig1.update_layout(width=800)
+    # st.plotly_chart(fig1, use_container_width=False)
+    # fig2.update_layout(width=800)
+    # st.plotly_chart(fig2, use_container_width=False)
+
+    df_long = pd.wide_to_long(conditioned_df, stubnames='percentage', i=['country','date'],j='param',sep='_', suffix='\w+').reset_index()
+    df_long = df_long.sort_values(by='date')
+    fig = px.bar(df_long, x='country', y='percentage', animation_frame='date', color='param', barmode='group', range_y=[0,60])
+    #fig = px.bar(conditioned_df, x='country', y=['percentage_active', 'percentage_vaccinated'], animation_frame='date', barmode='group')
+    fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 10
+    fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 4
+    fig.update_layout(width=800)
+    st.plotly_chart(fig, use_container_width=False)
 
 st.write("""
 #### Take turns to see the vaccination percentage and active case count across countries at different dates
@@ -40,26 +83,9 @@ count = country_df[(country_df['country'] == option) & (country_df['date'] == pd
 # Displaying the results
 st.write("The total vaccinated people are " + str(vacc) + ", the vaccination percentage as of " + str(date) + " is " + str(per) + "% yet the active cases count is " + str(count))
 
-st.write(
-""" #### Isn't that a slow process, let's compare the timeseries data between countries at once...
-""")
-# Country wise comparison bar graphs
-countries = st.multiselect("Which countries do you like to see?", country_list, ['United States', 'India', 'Brazil'])
-date = st.selectbox("Pick a date", options = ['autoplay'] + list(vaccine_df[vaccine_df['country'].isin(countries)]['date'].unique()))
 
-# For a selected date
-if date != 'autoplay':
-    conditioned_df = vaccine_df[(vaccine_df['country'].isin(countries)) & (vaccine_df['date'] == pd.to_datetime(date))]
-    fig = px.bar(conditioned_df, x='country', y='percentage_vaccinated', color='country')
-
-# For all dates
-else:
-    conditioned_df = vaccine_df[(vaccine_df['country'].isin(countries))]
-    conditioned_df['date'] = pd.to_datetime(conditioned_df['date']).dt.strftime('%Y-%m-%d')
-    fig = px.bar(conditioned_df, x='country', y='percentage_vaccinated', range_y=[0, 100], color='country', animation_frame='date', animation_group='country')
-    fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 5
-    fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 2
-
-# Displaying the graph
-fig.update_layout(width=800)
-st.write(fig)
+vaccine_df['date'] = pd.to_datetime(vaccine_df['date']).dt.strftime('%Y-%m-%d')
+vaccine_df = vaccine_df[(vaccine_df['country'].isin(countries))]
+figcovid = px.scatter(vaccine_df, x="percentage_vaccinated", y="percentage_active", animation_frame="date", 
+    animation_group="country", range_x=[0,70], range_y=[0,10], color='country')
+st.plotly_chart(figcovid, use_container_width=False)
