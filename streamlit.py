27e8080487af9b5c109266224d096b076264c1c1
Sundar <43403815+SundarAnand@users.noreply.github.com>
2021-10-06 20:26:45 -0400

    Added viz

--- streamlit.py
+++ streamlit.py
@@ -7,52 +7,59 @@ import plotly.express as px
 # Introduction
 st.write(""" 
 # Interactive Data Science
-## By Annie Johnson and Sundar Anand
-## Interesting trends when pandemic happened""")
+### By Annie Johnson and Sundar Anand
+### Interesting trends when pandemic happened""")
 
 # Reading the file
 country_df = pd.read_csv("vaccination dataset/country.csv")
+country_df['date'] = pd.to_datetime(country_df['date']).dt.date
 
 # Getting only vaccination data
 vaccine_df = country_df[(country_df['people_vaccinated'].notna())]
-vaccine_df['date'] = pd.to_datetime(vaccine_df['date']).dt.date
 vaccine_df = vaccine_df.sort_values(by='date')
 
 # Getting the country list
 country_list = sorted(vaccine_df['country'].unique())
 
+st.write("""
+#### Take turns to see the vaccination percentage and active case count across countries at different dates
+""")
 # Getting country and date as input
-option = st.selectbox('Pick a country', country_list)
+default = country_list.index('United States')
+option = st.selectbox('Pick a country', country_list, index=default)
 country_vaccine_df = vaccine_df[(vaccine_df['country'] == option)]
-date = st.select_slider("Pick a date", options = country_vaccine_df['date'].unique())
+date = st.select_slider("Pick a date", options = country_vaccine_df[country_vaccine_df['active_cases'].notna()]['date'].unique())
 
 # Calculating the percentage of vaccination
 
 vacc_info_df = country_vaccine_df[(country_vaccine_df['date'] == pd.to_datetime(str(date)))]
 vacc = vacc_info_df['people_vaccinated'].iloc[0]
-per = round(vacc*100/vacc_info_df['population'].iloc[0], 2)
+per = round(vacc_info_df['percentage_vaccinated'].iloc[0], 2)
+count = country_df[(country_df['country'] == option) & (country_df['date'] == pd.to_datetime(str(date)))]['active_cases'].iloc[0]
 
 # Displaying the results
-st.write("The total vaccinated people are " + str(vacc) + " and the vaccination percentage as of " + str(date) + " is " + str(per) + "%")
+st.write("The total vaccinated people are " + str(vacc) + ", the vaccination percentage as of " + str(date) + " is " + str(per) + "% yet the active cases count is " + str(count))
 
+st.write(
+""" #### Isn't that a slow process, let's compare the timeseries data between countries at once...
+""")
 # Country wise comparison bar graphs
-vaccine_df['date'] = pd.to_datetime(vaccine_df['date']).dt.strftime('%Y-%m-%d')
-countries = st.multiselect("Which country do you like to see?", country_list, ['United States'])
+countries = st.multiselect("Which countries do you like to see?", country_list, ['United States', 'India', 'Brazil'])
 date = st.selectbox("Pick a date", options = ['autoplay'] + list(vaccine_df[vaccine_df['country'].isin(countries)]['date'].unique()))
 
 # For a selected date
 if date != 'autoplay':
     conditioned_df = vaccine_df[(vaccine_df['country'].isin(countries)) & (vaccine_df['date'] == pd.to_datetime(date))]
-    fig = px.bar(conditioned_df, x='country', y='people_vaccinated', color='country')
+    fig = px.bar(conditioned_df, x='country', y='percentage_vaccinated', color='country')
 
 # For all dates
 else:
     conditioned_df = vaccine_df[(vaccine_df['country'].isin(countries))]
-    #conditioned_df['date'] = pd.to_datetime(conditioned_df['date']).dt.strftime('%Y-%m-%d')
-    fig = px.bar(conditioned_df, x='country', y='people_vaccinated', range_y=[0, max(conditioned_df['people_vaccinated'])], color='country', animation_frame='date', animation_group='country')
-    fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 10
-    fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 4
-    fig.update_layout(width=1000)
+    conditioned_df['date'] = pd.to_datetime(conditioned_df['date']).dt.strftime('%Y-%m-%d')
+    fig = px.bar(conditioned_df, x='country', y='percentage_vaccinated', range_y=[0, 100], color='country', animation_frame='date', animation_group='country')
+    fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 5
+    fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 2
 
 # Displaying the graph
+fig.update_layout(width=800)
 st.write(fig)
